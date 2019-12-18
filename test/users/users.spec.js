479cf4c99e061d6332066ca0498c260d30985c58
jae young Ahn <easycodingkr@gmail.com>
2019-12-18 15:49:57 +0900

    promise bug fixed in tests

--- test/users/users.spec.js
+++ test/users/users.spec.js
@@ -12,9 +12,13 @@ describe("Test 'users' service", () => {
   beforeAll(() => broker.start())
   afterAll(() => broker.stop())
   const removeUserByEmail = async (email) => {
-    const usersToRemove = await broker.call("users.find",{ email: email })
-    usersToRemove.map((user) => broker.call("users.remove",{ id:  user._id}))
-    await broker.call("users.find",{ email: 'test@gmail.com' })    
+    const usersToRemove = await broker.call("users.find",{ email: email })    
+    Promise.all( // https://www.taniarascia.com/promise-all-with-async-await/
+      usersToRemove.map(async user => {
+        await broker.call("users.remove",{ id:  user._id})
+      })
+    )    
+    const result = await broker.call("users.find",{ email: email })        
   }
 
   describe("Test 'user service' is available", () => {
@@ -31,15 +35,43 @@ describe("Test 'users' service", () => {
       expect(_.find(broker.services,{ name: "users" })).toHaveProperty('validateEntity')
     })    
     it("should return with token string", async () => {
-      const email = 'test@gmail.com'      
+      const email = 'test1@gmail.com'      
       removeUserByEmail(email)      
       expect(broker.call('users.signup', { email: email, password: 'asdf1004' })).resolves.toContain('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9')
+    })
+
+    it("should reject by client error if an user already registered", async () =>{
+      const email = 'test@gmail.com'       
+      removeUserByEmail(email)
+      await expect(broker.call('users.signup', { email: email, password: 'asdf1004' })).resolves.toContain('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9')
+      await expect(broker.call('users.signup', { email: email, password: 'asdf1004' })).rejects.toBeInstanceOf(MoleculerClientError)
     })
     it("should reject an ValidationError", () => {
       expect(broker.call('users.signup', {})).rejects.toBeInstanceOf(ValidationError)
     })    
   })
-  describe("Test 'JWT generate' method", () => {
+  
+  
+  describe("Test 'users.signin' action", () => {
+
+    it("should rejects by not found user", async () => {            
+      const userEntity = {
+        email: 'test32123@gmail.com',
+        password: 'asdf1002312'
+      }
+      await removeUserByEmail(userEntity.email) // delete email 
+      expect(broker.call('users.signin', { email: userEntity.email, password: userEntity.password })).rejects.toBeInstanceOf(MoleculerClientError)
+    })
+    it("should reject an ValidationError", () => {
+      expect(broker.call('users.signup', {})).rejects.toBeInstanceOf(ValidationError)
+    })       
+    it("should signin returned a token ", () => {
+      // todo signup with email/password
+      // todo signin with email/password
+      // remove user by email.
+    })
+  })
+  describe("Test 'JWT generation' method", () => {
     it("should have generatedJWT method in service", () => {
       expect(userService).toHaveProperty('generatedJWT')
     })
@@ -50,26 +82,6 @@ describe("Test 'users' service", () => {
         password: 'asdf10023'
       }
       expect(userService.generatedJWT(userEntity)).toContain('yJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9')
-    })
-  })
-  
-  describe("Test 'users.signin' action", () => {
-
-    it("should rejects by not found user", () => {            
-      const userEntity = {
-        email: 'easycodingkr@gmail.com',
-        password: 'asdf1002312'
-      }
-      removeUserByEmail(userEntity.email) // delete email 
-      expect(broker.call('user.signin', userEntity)).rejects.toBeInstanceOf(MoleculerClientError)
-    })
-    it("should reject an ValidationError", () => {
-      expect(broker.call('users.signup', {})).rejects.toBeInstanceOf(ValidationError)
-    })       
-    it("should signin returned a token ", () => {
-      // todo signup with email/password
-      // todo signin with email/password
-      // remove user by email.
     })
   })
 })
